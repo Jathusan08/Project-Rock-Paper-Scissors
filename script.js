bc76cee7fb68d347518ae30298bfca401770e8d2
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2023-04-22 00:07:29 +0100

    Complete console based game

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+//              P C D
+let results = [0, 0, 0];
+
 function getComputerChoice() {
   let computerChoice;
+
   switch (Math.floor(Math.random() * 3 + 1)) {
     case 1:
       computerChoice = "Rock";
@@ -21,69 +25,64 @@ function getComputerChoice() {
 }
 
 function playSingleRound(playerSelection, computerSelection) {
-  let message;
-  let playerScore = 0;
-  let computerScore = 0;
-
   if (playerSelection.charAt(0).toUpperCase() === computerSelection.charAt(0)) {
-    message = `Draw`;
-    // console.log(message);
+    console.log(`Draw`);
+    results[2] += 1;
   } else if (
     playerSelection.charAt(0).toUpperCase() +
       playerSelection.slice(1).toLowerCase() ===
       "Rock" &&
     computerSelection === "Paper"
   ) {
-    computerScore += 1;
-    //console.log("You Lose! Paper beats Rock");
-    message = "You Lose! Paper beats Rock";
+    console.log("You Lose! Paper beats Rock");
+    results[1] += 1;
+    //  message = "You Lose! Paper beats Rock";
   } else if (
     playerSelection.charAt(0).toUpperCase() +
       playerSelection.slice(1).toLowerCase() ===
       "Paper" &&
     computerSelection === "Rock"
   ) {
-    playerScore += 1;
-    // console.log("You Win! Paper beats Rock");
-    message = "You Win! Paper beats Rock";
+    results[0] += 1;
+    console.log("You Win! Paper beats Rock");
+    //message = "You Win! Paper beats Rock";
   } else if (
     playerSelection.charAt(0).toUpperCase() +
       playerSelection.slice(1).toLowerCase() ===
       "Rock" &&
     computerSelection === "Scissors"
   ) {
-    playerScore += 1;
-    //   console.log("You Win! Rock beats Scissors");
-    message = "You Win! Rock beats Scissors";
+    results[0] += 1;
+    console.log("You Win! Rock beats Scissors");
+    // message = "You Win! Rock beats Scissors";
   } else if (
     playerSelection.charAt(0).toUpperCase() +
       playerSelection.slice(1).toLowerCase() ===
       "Scissors" &&
     computerSelection === "Rock"
   ) {
-    computerScore += 1;
-    // console.log("You Lose! Rock beats Scissors");
-    message = "You Lose! Rock beats Scissors";
+    console.log("You Lose! Rock beats Scissors");
+    results[1] += 1;
+    // message = "You Lose! Rock beats Scissors";
   } else if (
     playerSelection.charAt(0).toUpperCase() +
       playerSelection.slice(1).toLowerCase() ===
       "Paper" &&
     computerSelection === "Scissors"
   ) {
-    computerScore += 1;
-    //console.log("You Lose! Scissors beats Rock");
-    message = "You Lose! Scissors beats Rock";
+    console.log("You Lose! Scissors beats Rock");
+    results[1] += 1;
+    // message = "You Lose! Scissors beats Rock";
   } else if (
     playerSelection.charAt(0).toUpperCase() +
       playerSelection.slice(1).toLowerCase() ===
       "Scissors" &&
     computerSelection === "Paper"
   ) {
-    playerScore += 1;
-    //   console.log("You Win! Scissors beats Rock");
-    message = "You Win! Scissors beats Rock";
+    results[0] += 1;
+    console.log("You Win! Scissors beats Rock");
+    // message = "You Win! Scissors beats Rock";
   }
-  return message;
 }
 
 //console.log(playSingleRound("Rock", getComputerChoice()));
@@ -96,7 +95,15 @@ function play5Rounds() {
   for (let i = 1; i < 6; i++) {
     let input = prompt("Please choose Rock or Paper Or Scissors");
 
-    console.log(playSingleRound(input, getComputerChoice()));
+    playSingleRound(input, getComputerChoice());
+  }
+
+  if (results[0] === results[1]) {
+    console.log(`It is a Draw game`);
+  } else if (results[0] > results[1]) {
+    console.log(`Player won and defeated Computer by ${results[0]} points`);
+  } else {
+    console.log(`Computer won and defeated Player by ${results[1]} points`);
   }
 }
 
