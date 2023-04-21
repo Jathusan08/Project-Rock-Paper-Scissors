f6b592faab9d7f638b1a3451d4602d8193ab21f2
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2023-04-21 20:19:03 +0100

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,23 @@
+function getComputerChoice() {
+  let computerChoice;
+  switch (Math.floor(Math.random() * 3 + 1)) {
+    case 1:
+      computerChoice = "Rock";
+
+      break;
+
+    case 2:
+      computerChoice = "Paper";
+
+      break;
+
+    case 3:
+      computerChoice = "Scissors";
+
+      break;
+  }
+
+  return computerChoice;
+}
+
+console.log(getComputerChoice());
